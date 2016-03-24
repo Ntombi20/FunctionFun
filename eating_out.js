050644dcdd180f39cca789352ea73dd56a74476e
Ntombi20 <ntombi@projectcodex.co>
2016-03-24 15:11:12 +0200

    done

--- eating_out.js
+++ eating_out.js
@@ -5,31 +5,35 @@ function teamEatingOut(eatingOutString){
 
 	   var outList = [];
 	   for(var i=0; i<out.length; i++){
-	   var food = out[i] 
+	   var food = out[i]; 
 
        var foodString = food
             .replace(" Your team ate ", "")
             .replace(" drank ", "")
             .replace(" and ", ",")
-            .trim();
-
-        var item = foodString
-            .split(",");
-            console.log(item);
+            .split(",")
+        } 
+            for (var n = 0; n < foodString.length; n++) {
+                var newString = foodString[n].split(" ");
+                console.log(foodString[n].split(" "));
+            }     
+        // var item1 = foodString[0].split(" ");
+        // var item2 = foodString[1].split(" "); 
+        // var item3 = foodString[2].split(" ");    
+         
+        // var noOfItem = Number(item1[0])
+        //                // Number(item2[0])
+        //                // Number(item3[0]); 
+        
+        // var foods = item1[1]
+        //             // item2[1]
+        //             // item3[1];        
        
-        // var foodName = [1];
-        // if (isNaN(foodParts[0])){
-        //     foodName = foodParts[1] + " " + foodParts[1];
-        // }
-        // var noOfItem = parseInt(foodParts[foodParts.length]);
-
-
- //        outList.push({
- //          food : foodName,
- //          noOfItem : noOfItem
- //        })
-        }
-
+        // outList.push({
+        //     setOfItem: setOfItem,
+        //     foods: foods
+        
+         
 	return outList;
-};
+}
 
