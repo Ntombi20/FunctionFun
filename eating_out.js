38537b9eac347ae2718f3cf27a4ddc7e9e64daca
Ntombi20 <ntombi@projectcodex.co>
2016-03-29 15:41:20 +0200

    done

--- eating_out.js
+++ eating_out.js
@@ -24,12 +24,73 @@ function teamEatingOut(eatingOutString){
             var noOfItem = Number(item[0]);
 
             foodList.push({
-                    noOfItem: noOfItem,
-                    food: food
+                noOfItem: noOfItem,
+                food: food
             })
-
-           
-        } 
-         console.log(foodList)          
+        }           
 	return foodList;
 }
+
+function prices(pricesString){
+
+    var string = pricesString.split(":").slice(1);
+
+    var priceList = [];
+    for (var i = 0; i < string.length; i++) {
+        var newSting = string[i]
+            .replace("each", "")
+            .trim()
+            .split(", ");   
+    }
+
+    for (var i = 0; i < newSting.length; i++) {
+        newSting[i]
+
+        var itemNdCost = newSting[i].split(" - ");
+
+        var food = itemNdCost[0];
+
+        var cost = Number(itemNdCost[1]);
+
+        priceList.push({
+            food: food,
+            cost: cost
+        })
+    }
+    
+    return priceList;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var prices = costString
+//             .split("R").join(" ").split(" ").slice(" ");
+            
+//          console.log(prices);
+
+//         var cost1 = Number(prices[1]); 
+//         var cost2 = Number(prices[2]);
+//         var cost3 = Number(prices[3]);
+
+//         var AllCost = (cost1 * 3) + (cost2 * 3) + (cost3 * 3);
+//         costList.push(
+//             AllCost
+//             );
--- eating_out_test.js
+++ eating_out_test.js
@@ -1,4 +1,7 @@
-var expectedPlaces = [
+QUnit.test("find the out what team ate and drank", function(assert) {
+	var team = teamEatingOut("Hi Xola thanks for joining us last night. Your team ate 8 burgers, drank 17 beers and 11 fanta's");
+
+	var expectedPlaces = [
 	  {
 	    "food": "burgers",
 	    "noOfItem": 8
@@ -12,31 +15,29 @@ var expectedPlaces = [
 	    "noOfItem": 11
 	  }
 	]
-
-QUnit.test("find the out what team ate and drank", function(assert) {
-	var team = teamEatingOut("Hi Xola thanks for joining us last night. Your team ate 8 burgers, drank 17 beers and 11 fanta's");
-
-		assert.deepEqual(team, expectedPlaces);
+	
+	assert.deepEqual(team, expectedPlaces);
 	})
+
 
 QUnit.test("how much it cost", function(assert) {
 
 	var expectedCost = [
 		{
 		    "food": "burgers",
-		    "cost": 296
+		    "cost": 37
 		},
 		{
 			"food": "beers",
-			"cost": 204
+			"cost": 12
 		},
 		{
 			"food": "fanta's",
-			"cost": 99
+			"cost": 9
 		}
 	]
 
-	assert.equal(prices("Please send me the payment via EFT prices are as follows: burgers - 37 each, beers - 12, fanta's 9"), expectedCost);
+	assert.deepEqual(prices("Please send me the payment via EFT prices are as follows: burgers - 37 each, beers - 12, fanta's - 9"), expectedCost);
 })
 
 
